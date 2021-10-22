a09fc2c9972d880205b4141cc46d9da5942d1332
kev <kevHTW@gmx.com>
2021-10-22 19:46:03 +0200

    init spring

---
+++ src/main/java/com/kbe/LC2/Lc2Application.java
@@ -0,0 +1,13 @@
+package com.kbe.LC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbe/LC2/Lc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbe.LC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
